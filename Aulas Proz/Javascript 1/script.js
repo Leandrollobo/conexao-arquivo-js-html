e734b4edc0eeccec0d5b13bcf39e657238c821c7
Leandro L. Lobo <148393517+Leandrollobo@users.noreply.github.com>
2023-11-25 21:20:37 -0300

    Add files via upload

---
+++ Aulas Proz/Javascript 1/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+// let n1 = 1;
+// let n2 = 1;
+// console.log(n1 + n2);
+
+function texto() {
+  console.log("Conexão feita com sucesso!");
+}
+
+texto();
+texto();
+texto();
